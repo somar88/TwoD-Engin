5927e9da72a6cfb13a7885f49e12fcbd00147403
D065922 <D065922@WDFN33991635A.emea.global.corp.sap>
2017-09-06 17:48:08 +0200

    src

--- Work-Test/src/com/abraham/gfx/Sprite.java
+++ Work-Test/src/com/abraham/gfx/Sprite.java
@@ -6,18 +6,23 @@ public class Sprite {
 	public int sprite_length = 32;
 	public int sprite_XPos;
 	public int sprite_YPos;
-	public int[] sprite;
+	public int[] sprite = new int[sprite_width * sprite_length];
 
 	public Sprite(SpriteSheet s_Sheet, int sprite_XPos, int sprite_YPos) {
+
 		load_sprite(s_Sheet, sprite_XPos, sprite_YPos);
 	}
 
 	private void load_sprite(SpriteSheet s_Sheet, int sprite_XPos, int sprite_YPos) {
-		for (int x = 0; x < sprite_width; x++) {
-			for (int y = 0; y < sprite_length; y++) {
-				sprite[x + (y * sprite_width)] = s_Sheet.sheet_pixels[((sprite_XPos * sprite_width) + x)
-						+ ((sprite_YPos * sprite_width) + y)];
+		if (s_Sheet.sheet_pixels != null) {
+			for (int x = 0; x < sprite_width; x++) {
+				for (int y = 0; y < sprite_length; y++) {
+					sprite[x + (y * sprite_width)] = s_Sheet.sheet_pixels[((sprite_XPos * sprite_width) + x)
+							+ ((sprite_YPos * sprite_width) + y) * s_Sheet.sheet_width];
+				}
 			}
+		} else {
+			System.out.println("There was no Sprit sheet loaded, check again!");
 		}
 	}
 
--- Work-Test/src/com/abraham/gfx/SpriteSheet.java
+++ Work-Test/src/com/abraham/gfx/SpriteSheet.java
@@ -1,25 +1,33 @@
 package com.abraham.gfx;
 
-import java.awt.image.DataBufferInt;
+import java.awt.image.BufferedImage;
 import java.net.URL;
+
 import javax.imageio.ImageIO;
 
 public class SpriteSheet {
 
+	public URL sheet_url;
 	public int sheet_width;
 	public int sheet_height;
+	private BufferedImage sheet_img;
 	public int[] sheet_pixels;
-	public URL sheet_url;
+
+	public static SpriteSheet SS = new SpriteSheet("/SpreiteSheets/MiscDummy.png");
 
 	public SpriteSheet(String path) {
-		
+
 		try {
-			sheet_url = new URL(path);
-			this.sheet_height = ImageIO.read(sheet_url).getHeight();
-			this.sheet_width = ImageIO.read(sheet_url).getWidth();
-			sheet_pixels = ((DataBufferInt) (ImageIO.read(sheet_url)).getRaster().getDataBuffer()).getData();
+			sheet_img = ImageIO.read(SpriteSheet.class.getResourceAsStream(path));
+			sheet_height = sheet_img.getHeight();
+			sheet_width = sheet_img.getWidth();
+			sheet_pixels = new int[sheet_height * sheet_width];
+			sheet_img.getRGB(0, 0, sheet_img.getWidth(), sheet_img.getHeight(), sheet_pixels, 0, sheet_img.getWidth());
+			System.out.println("Sprite sheet loaded and ready.\n");
 		} catch (Exception e) {
-			System.out.println("Image not found, please provide a valid URL");
+
+			System.out.println("Error by Loading Spritesheet image:\n");
+			System.out.println(e.getMessage());
 		}
 	}
 
